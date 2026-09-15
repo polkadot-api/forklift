b9fdb7e9c818f487d0f2bf477457e66cae9151c6
Victor Oliva <olivarra1@gmail.com>
2026-09-15 16:07:00 +0200

    Revert "feat: add WS ping frames"
    This reverts commit 17ec4b0c9f48dae7f84efc3a6daaf9fded662d24.

--- server/node.ts
+++ server/node.ts
@@ -35,25 +35,8 @@ export const createWsServer = async (
         }
       });
 
-      let alive = true;
-      ws.on("pong", () => {
-        alive = true;
-      });
-      const pingInterval = setInterval(() => {
-        if (!alive) {
-          clearInterval(pingInterval);
-          ws.terminate();
-          return;
-        }
-
-        alive = false;
-        ws.ping();
-      }, 10_000);
-      ws.ping();
-
       ws.on("close", () => {
         connection.disconnect();
-        clearInterval(pingInterval);
       });
     });
 
